77d7c6ea4018fe8b90000c6ee6961a5ae68b6ae1
Burak <burakguven351999@gmail.com>
2022-11-19 17:55:26 +0300

    IAuthService oluşturuldu video izliyorum devam edilecek

---
+++ BusinessLayer/Abstract/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IAuthService
+    {
+        IDataResult<Customer> Register(); // parantez içine dto oluştur yaz
+    }
+}
