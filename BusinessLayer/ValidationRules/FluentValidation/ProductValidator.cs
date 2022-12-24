088d5f118185011f56972e3c6eaf3f767b96c66f
Burak GÜVEN <190601075@bakircay.edu.tr>
2022-12-24 17:57:12 +0300

    validate eklendi

--- BusinessLayer/ValidationRules/FluentValidation/ProductValidator.cs
+++ BusinessLayer/ValidationRules/FluentValidation/ProductValidator.cs
@@ -17,7 +17,7 @@ namespace BusinessLayer.ValidationRules.FluentValidation
             RuleFor(p => p.ProductName).MinimumLength(2);
             RuleFor(p => p.UnitsInStock).NotEmpty().WithMessage("Ürünün stok adetini belirtiniz !");
             RuleFor(p => p.UnitPrice).NotEmpty();
-            RuleFor(p => p.UnitsInStock).NotEmpty()
+            RuleFor(p => p.UnitsInStock).NotEmpty();
             RuleFor(p => p.UnitPrice).GreaterThan(0).WithMessage("Fiyat 0'dan büyük olmalıdır!");
             RuleFor(p => p.ProductPhoto).NotEmpty().WithMessage("Fotoğraf yükleyiniz !");
 
